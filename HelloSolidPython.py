0418c171ec3524382b4f1faef6d8ec76bce8b30a
Roger Cheng <roger.random@outlook.com>
2018-01-17 20:02:25 -0800

    Correct syntax errors

--- HelloSolidPython.py
+++ HelloSolidPython.py
@@ -1,9 +1,9 @@
 from solid import *
 from solid.utils import *
 
-difference() {
-  cube(10)
+d = difference() (
+  cube(10),
   sphere(15)
-}
+)
 
-print scad_render(py_scad_obj)
+print(scad_render(d))
